57bd7aeee7bbcf15863740d95a5bf4f109a768d6
rathanr <rathanr@MAVCHN1121234.mavericsystems.lan>
2024-05-27 11:05:07 +0530

    Laptop change commit

---
+++ TestNG/src/Practise/day1.java
@@ -0,0 +1,24 @@
+package Practise;
+
+import org.testng.annotations.Test;
+
+public class day1 {
+
+	@Test  // 1st Test case (Method)
+	public void Demo() 
+	{	
+		System.out.println("Rathan Rajiv Singh");
+	}
+	
+	@Test  // 2nd Test case (Method)
+	public void DemoSecond()
+	{
+		System.out.println("Jack Singh Dharma");
+	}
+
+	@Test  // 3rd Test case (Method)
+	public void DemoThird()
+	{
+		System.out.println("The King");
+	}
+}
---
+++ TestNG/src/Practise/day2.java
@@ -0,0 +1,18 @@
+package Practise;
+
+import org.testng.annotations.Test;
+
+public class day2 {
+	
+	@Test // 1st Test case (Method)
+	public void demo2()
+	{
+		System.out.println("Bathmasingh Selva Meeran");
+	}
+	
+	@Test // 2nd Test case (Method)
+	public void demo3()
+	{
+		System.out.println("Bathmasingh Selva Meeran");
+	}
+}
